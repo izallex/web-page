a5c782c1c6b56725afa9827efbdfba7d8e98e945
hirsch <st167295@stud.uni-stuttgart.de>
2022-02-22 20:56:31 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,79 @@
 import rightTag from './right_tag.svg';
 import leftTag from './left_tag.svg';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { Link, Routes, Route, BrowserRouter } from "react-router-dom";
+import Impressum from './Impressum.js'
 import './App.css';
 
 function App() {
-  return (
-    <div>
-    <header>
-    <div style={{display:'flex', flexDirection:'row', marginTop:'2%'}}>
-      <div style={{width:'50%'}}>
-    <img src ={leftTag} style={{width:'40%',float:'right'}} /></div>
-    <div  style={{fontSize: '300%', fontStyle:'oblique', color:'white', width:'21%',float:'right'}}>
-     Web Design
-    </div>
-    <div style={{width:'20%',float:'left'}}>
-       <img src ={rightTag} style={{width:'70%'}} /> </div>
-    <div style={{float:'left', height:'50%',fontSize: '100%', fontStyle:'oblique', color:'white', width:'30%'}}>
-     by Alexandra Hirsch
-    
-     </div>
-    
+	return (
+		<div>
+			<BrowserRouter>
+				<header style={{ display: 'flex', flexDirection: 'row' }}>
 
-   </div>
-   </header>
-   <main>
+					<div style={{ width: '43%' }}>
+						<img src={leftTag} style={{ float: 'right' }} />
+					</div>
+					<div style={{ float: 'right' }}>
+						<h1 style={{ fontStyle: 'oblique', color: 'white', marginLeft: '4%' }} >Webdesign</h1>
+					</div>
+					<div style={{ float: 'left' }}>
+						<img src={rightTag} />
+					</div>
+					<div style={{ float: 'left', height: '50%', fontSize: '100%', fontStyle: 'oblique', color: 'white', marginTop: '1%' }}>
+						by Alexandra Hirsch
+					</div>
+					{/*<nav>
+						<Link to="/web-design">
+							Home
+						</Link>
+						<Link to="/training">
+							Training
+						</Link>
+						<Link to="/datenupload">
+							Datasets
+						</Link>
+						<Link to="/evaluation">
+							Evaluation
+						</Link>
+						<Link to="/help">
+							Help
+						</Link>
+					</nav> */}
+				</header>
+				<main>
 
-   </main>
-   <footer>
+					<Routes>
+						<Route exact path="/web-design">
 
-   </footer>
-   </div>
-  );
+						</Route>
+						<Route exact path="/training">
+
+						</Route>
+						<Route exact path="/datenupload">
+
+						</Route>
+						<Route exact path="/evaluation" onClick={(e) => e.preventDefault()}>
+
+						</Route>
+
+						<Route exact path="/help">
+
+						</Route>
+
+						<Route exact path="/web-design/impressum" element={<Impressum/>}>
+                      
+						</Route>
+					</Routes>
+				</main>
+				<footer>
+					<Link to="/web-design/impressum" className="impressumLink">
+						<u style={{ float: 'right', marginRight: '4%', color: 'white' }}>Impressum</u>
+					</Link>
+				</footer></BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Impressum.js
@@ -0,0 +1,11 @@
+import React from "react";
+class Impressum extends React.Component {
+    render() {
+      return (
+      <div style={{color:'white', marginLeft:'10%'}}>
+          <h2>Impressum</h2>
+</div>
+      )
+    }
+  }
+  export default Impressum;
