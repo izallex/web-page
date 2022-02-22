87e0924a8df095ddf8705236e0fca2c72d188da8
hirsch <st167295@stud.uni-stuttgart.de>
2022-02-22 21:04:41 +0100

    impressum

--- src/Impressum.js
+++ src/Impressum.js
@@ -3,7 +3,23 @@ class Impressum extends React.Component {
     render() {
       return (
       <div style={{color:'white', marginLeft:'10%'}}>
-          <h2>Impressum</h2>
+         <h1>Impressum</h1>
+
+<h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+<p>Alexandra Hirsch<br />
+Webdesign N&uuml;rtingen - Alexandra Hirsch<br />
+Eberhardstr. 10<br />
+72622 N&uuml;rtingen</p>
+
+<h2>Kontakt</h2>
+<p>Telefon: 07022/39952<br />
+E-Mail: alexandra.hirsch.nt@gmail.com</p>
+
+<h2>EU-Streitschlichtung</h2>
+<p>Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a style={{color:'grey'}} href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
+
+<h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+<p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
 </div>
       )
     }
